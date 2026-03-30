2f552a31f8ce1dd1b50e7588a243ce2dc3438aa4
Nasser Aladawani <na98848455@gmail.com>
2026-03-30 12:23:49 +0400

    day1

--- Day1/ip_validator.py
+++ Day1/ip_validator.py
@@ -1,31 +1,37 @@
-while(True):
-    def validIP(ip):
 
-        parts = ip.split(".")
+def validIP(ip):
 
-        if len(parts) != 4:
-            print("IP address must contain exactly four octets separated by periods (.) ")
+    parts = ip.split(".")
+
+    if len(parts) != 4:
+        print("{parts} : IP address must contain exactly four octets separated by periods (.) ")
+        return False
+    
+    for i in parts:
+        if not i.isdigit():
+            print("{parts} : IP address must consist only of digits (no letters or special characters).")
             return False
         
-        for i in parts:
-            if not i.isdigit():
-                print("IP address must consist only of digits (no letters or special characters).")
-                return False
-            
-            num = int(i)
+        num = int(i)
 
-            if num < 0 or num > 255:
-                print("IP address must be a number between 0 and 255 (inclusive).")
-                return False
-        
-        else:
-            print("IP Address is correct!!!")
-            return True
-        
+        if num < 0 or num > 255:
+            print("{parts} : IP address must be a number between 0 and 255 (inclusive).")
+            return False
+    
+    else:
+        print(f"{parts} : IP Address is correct!!!")
+        return True
+    
 
 
+while True:
     user_i = input("enter an ip address:   ")
+
+    if user_i == "":
+        break
+
     validIP(user_i)
+    print()
 
             
 
